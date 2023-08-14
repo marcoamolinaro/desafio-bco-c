d1f83b1896140b04018da471892e577208727fdf
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-08-14 15:07:30 -0300

    Primeiro commit

---
+++ LancamentoService/src/main/java/br/com/carrefour/lancamentoservice/LancamentoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.carrefour.lancamentoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LancamentoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancamentoServiceApplication.class, args);
+	}
+
+}
---
+++ LancamentoService/src/test/java/br/com/carrefour/lancamentoservice/LancamentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carrefour.lancamentoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LancamentoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
