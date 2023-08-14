4ceecadd4c9c0aa3a684eef87d14530c411d71c7
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-08-14 18:11:16 -0300

    ServiceRegistry criado para service discovery

---
+++ ServiceRegistry/src/test/java/br/com/carrefour/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carrefour.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
